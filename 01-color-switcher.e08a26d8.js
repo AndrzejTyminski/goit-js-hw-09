faf1f8cfef995cd76b17b8d58853c9a5abc09331
AndrzejTyminski <117731002+AndrzejTyminski@users.noreply.github.com>
2023-04-18 20:05:17 +0000

    Deploying to gh-pages from @ AndrzejTyminski/goit-js-hw-09@11857cc504c26f015d015a119ed301b6405a5df0 🚀

---
+++ 01-color-switcher.e08a26d8.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop"),o=document.querySelector("body");let r=null;function l(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}e.addEventListener("click",(()=>{r=setInterval((()=>{o.style.backgroundColor=l()}),1e3),e.disabled=!0})),t.addEventListener("click",(()=>{clearInterval(r),console.log(`Interval with id ${r} has stopped! with color ${l()}`),e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.e08a26d8.js.map
---
+++ 01-color-switcher.f620f01a.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop"),n=document.querySelector("body"),o=null;function c(){return"#".concat(Math.floor(16777215*Math.random()).toString(16))}t.addEventListener("click",(function(){o=setInterval((function(){n.style.backgroundColor=c()}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(o),console.log("Interval with id ".concat(o," has stopped! with color ").concat(c())),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.f620f01a.js.map
