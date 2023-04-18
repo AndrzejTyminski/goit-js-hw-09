11857cc504c26f015d015a119ed301b6405a5df0
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-04-18 22:04:56 +0200

    9.1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop');
+const bodyEl = document.querySelector('body');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  console.log(
+    `Interval with id ${timerId} has stopped! with color ${getRandomHexColor()}`
+  );
+  startBtn.disabled = false;
+});
