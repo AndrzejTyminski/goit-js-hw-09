69475c8c7e9c8d2be7de1b7c20b14076f08507ae
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-04-19 12:53:49 +0200

    9.3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,11 +1,29 @@
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', e => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  console.log(formData);
+  const firstDelay = parseInt(formData.get('delay'));
+  const step = parseInt(formData.get('step'));
+  const amount = parseInt(formData.get('amount'));
+  console.log(firstDelay, step, amount);
+  for (let i = 0; i < amount; i++) {
+    createPromise(i + 1, firstDelay + i * step);
+  }
+});
+
 function createPromise(position, delay) {
+  console.log(position, delay);
   return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      resolve();
-    } else {
-      reject();
-    }
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
 }
 
