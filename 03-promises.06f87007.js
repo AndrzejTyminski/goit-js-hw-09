912223134b6cb4f0579e75a55dba6e736ca3fb58
AndrzejTyminski <117731002+AndrzejTyminski@users.noreply.github.com>
2023-04-19 10:54:20 +0000

    Deploying to gh-pages from @ AndrzejTyminski/goit-js-hw-09@69475c8c7e9c8d2be7de1b7c20b14076f08507ae 🚀

--- 03-promises.06f87007.js
+++
@@ -1,2 +0,0 @@
-function o(o,e){return new Promise(((o,e)=>{Math.random()>.3?o():e()}))}o(),o().then((({position:o,delay:e})=>{console.log(`✅ Fulfilled promise ${o} in ${e}ms`)})).catch((({position:o,delay:e})=>{console.log(`❌ Rejected promise ${o} in ${e}ms`)}));
-//# sourceMappingURL=03-promises.06f87007.js.map
--- 03-promises.2e1b1a70.js
+++
@@ -1,2 +0,0 @@
-!function(){function n(n,o){return new Promise((function(n,o){Math.random()>.3?n():o()}))}n(),n().then((function(n){var o=n.position,c=n.delay;console.log("✅ Fulfilled promise ".concat(o," in ").concat(c,"ms"))})).catch((function(n){var o=n.position,c=n.delay;console.log("❌ Rejected promise ".concat(o," in ").concat(c,"ms"))}))}();
-//# sourceMappingURL=03-promises.2e1b1a70.js.map
