909087205c61b8bfe463f142da1abd98667850bd
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-04-19 13:03:29 +0200

    9.3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,4 @@
+import Notiflix from 'notiflix';
 const formEl = document.querySelector('.form');
 
 formEl.addEventListener('submit', e => {
@@ -9,12 +10,13 @@ formEl.addEventListener('submit', e => {
   const amount = parseInt(formData.get('amount'));
   console.log(firstDelay, step, amount);
   for (let i = 0; i < amount; i++) {
-    createPromise(i + 1, firstDelay + i * step)  .then(({ position, delay }) => {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
-    .catch(({ position, delay }) => {
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    });;
+    createPromise(i + 1, firstDelay + i * step)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 });
 
@@ -36,8 +38,8 @@ createPromise();
 
 createPromise(2, 1500)
   .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   })
   .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   });
