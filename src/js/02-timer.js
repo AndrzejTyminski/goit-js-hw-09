d01b5429b32528d564b7bacf7dd304fc1d828eb3
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-04-20 23:12:08 +0200

    2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -11,7 +11,6 @@ const secondsEl = document.querySelector('[data-seconds]');
 
 btnStartEl.disabled = true;
 
-
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -20,7 +19,7 @@ const options = {
   onClose(selectedDates) {
     if (selectedDates[0] < options.defaultDate) {
       btnStartEl.disabled = true;
-      alert('Nie wybieraj minionej daty');
+      alert('Please choose a date in the future');
     } else {
       btnStartEl.disabled = false;
     }
@@ -70,3 +69,6 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
